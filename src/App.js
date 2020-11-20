abec7eace1a1ce1d6c01e7691f0298757379f599
wozzy84 <piotrw84@gmail.com>
2020-11-20 19:25:06 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <h1>Hello Kasia!</h1>
+      <Button>Click</Button>
+    </>
+  );
+}
+
+export default App;
