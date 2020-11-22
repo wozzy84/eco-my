67db8e060b3be136f6f1766eae2bc445e7456aa2
wozzy84 <piotrw84@gmail.com>
2020-11-22 08:46:34 +0100

    final

--- src/components/oddaj/Give.js
+++ src/components/oddaj/Give.js
@@ -1,72 +1,78 @@
-import React, {useState} from "react";
-import {H3} from '../atoms/styles';
-import {GiveContainer, Items, ImgBoxContainer, Button, Img} from './styles';
+import React, { useState } from "react";
+import { H3 } from "../atoms/styles";
+import { GiveContainer, Items, ImgBoxContainer, Button, Img } from "./styles";
 import ImgBox from "./ImgBox";
-import medsPhoto from '../../../src/styles/assets/img/categories/leki-01.png';
-import clothesPhoto from '../../../src/styles/assets/img/categories/odziez-01.png';
-import plasticPhoto from '../../../src/styles/assets/img/categories/plastik-01.png';
-import batteriesPhoto from '../../styles/assets/img/categories/baterie-01.png';
-import teddyBearPhoto from '../../styles/assets/img/categories/zabawki-01.png';
+import medsPhoto from "../../../src/styles/assets/img/categories/leki-01.png";
+import clothesPhoto from "../../../src/styles/assets/img/categories/odziez-01.png";
+import plasticPhoto from "../../../src/styles/assets/img/categories/plastik-01.png";
+import batteriesPhoto from "../../styles/assets/img/categories/baterie-01.png";
+import teddyBearPhoto from "../../styles/assets/img/categories/zabawki-01.png";
 import GiveMap from "./GiveMap";
-const Give= () => {
-    // const [active, setActive] = useState(false);
-    const [types, setTypes] = useState([]);
-    const photos = [
-        {
-            url: medsPhoto,
-            name: 'elektrośmieci',
-            type: 'electro'
-        },
-        {
-            url: medsPhoto,
-            name: 'przeterminowane leki',
-            type: 'meds'
-        },
-        {
-            url: clothesPhoto,
-            name: 'stare ubrania',
-            type: 'clothes'
-        },
-        {
-            url: plasticPhoto,
-            name: 'nakrętki plastikowe',
-            type: 'caps'
-        },
-        {
-            url: batteriesPhoto,
-            name: 'baterie',
-            type: 'batteries'
-        },
-        {
-            url: teddyBearPhoto,
-            name: 'zabawki',
-            type: 'toys'
-        },
-    ];
+import Header from "../homepage/header/Header";
+const Give = () => {
+  // const [active, setActive] = useState(false);
+  const [types, setTypes] = useState([]);
+  const photos = [
+    {
+      url: medsPhoto,
+      name: "elektrośmieci",
+      type: "electro",
+    },
+    {
+      url: medsPhoto,
+      name: "przeterminowane leki",
+      type: "meds",
+    },
+    {
+      url: clothesPhoto,
+      name: "stare ubrania",
+      type: "clothes",
+    },
+    {
+      url: plasticPhoto,
+      name: "nakrętki plastikowe",
+      type: "caps",
+    },
+    {
+      url: batteriesPhoto,
+      name: "baterie",
+      type: "batteries",
+    },
+    {
+      url: teddyBearPhoto,
+      name: "zabawki",
+      type: "toys",
+    },
+  ];
 
-    return (
-        <>
-            <GiveContainer>
-                <H3 isWhite>Chcę oddać</H3>
-                <Items>
-                        <ImgBoxContainer>
-                            {photos.map((photo, index) => (
-                                <ImgBox
-                                    clicked={(active) => {
-                                      setTypes(active ? [...types, photo.type].slice(0) : types.filter(t => t !== photo.type).slice(0))
-                                    }}
-                                    key={index}
-                                    face={photo.url}
-                                    name={photo.name}
-                                />
-                            ))}
-                        </ImgBoxContainer>
-                </Items>
-
-            </GiveContainer>;
-            <GiveMap types={types} id='map'/>
-        </>
-        )
+  return (
+    <>
+      <Header />
+      <GiveContainer>
+        <H3 isWhite>Chcę oddać</H3>
+        <Items>
+          <ImgBoxContainer>
+            {photos.map((photo, index) => (
+              <ImgBox
+                clicked={(active) => {
+                  setTypes(
+                    active
+                      ? [...types, photo.type].slice(0)
+                      : types.filter((t) => t !== photo.type).slice(0)
+                  );
+                }}
+                key={index}
+                face={photo.url}
+                name={photo.name}
+              />
+            ))}
+          </ImgBoxContainer>
+        </Items>
+      </GiveContainer>
+      ;
+      <GiveMap types={types} id="map" />
+    </>
+  );
 };
 
 export default Give;
--- src/components/znajdzzbiorke/Find.js
+++ src/components/znajdzzbiorke/Find.js
@@ -1,88 +1,111 @@
-import React, {useState} from "react";
-import {GoogleMap, Marker, InfoWindow, withScriptjs, withGoogleMap} from "react-google-maps";
+import React, { useState } from "react";
+import {
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  withScriptjs,
+  withGoogleMap,
+} from "react-google-maps";
 
-import * as collectsData from '../../data/collects.json';
-import * as expiredDrugsData from '../../data/expired_drugs.json';
-import * as batteriesData from '../../data/batteries_accumulators.json';
-import * as wasteData from '../../data/waste_collecting_places.json';
-import * as realEstateData from '../../data/real_estate.json';
+import * as collectsData from "../../data/collects.json";
+import * as expiredDrugsData from "../../data/expired_drugs.json";
+import * as batteriesData from "../../data/batteries_accumulators.json";
+import * as wasteData from "../../data/waste_collecting_places.json";
+import * as realEstateData from "../../data/real_estate.json";
+import Header from "../homepage/header/Header.js";
 
-const mapCenter = {lat: 51.131271, lng: 23.4745222}; // Chełm city
-const googleMapsApiKey = 'AIzaSyDCYlmvsutOI1lUyi7gPJQlMEXqwTsdZQE';
+const mapCenter = { lat: 51.131271, lng: 23.4745222 }; // Chełm city
+const googleMapsApiKey = "AIzaSyDCYlmvsutOI1lUyi7gPJQlMEXqwTsdZQE";
 // const googleMapsApiKey = process.env.REACT_APP_GOOGLE_KEY;
 const googleMapURL = `https://maps.googleapis.com/maps/api/js?&key=${googleMapsApiKey}`;
 
-const Map = ({types}) => {
+const Map = ({ types }) => {
   const [selectedPlace, setSelectedPlace] = useState(null);
 
   const hasType = (type) => types && types.indexOf(type) > -1;
 
-  return <GoogleMap
-    defaultZoom={14}
-    defaultCenter={mapCenter}
-  >
-    {(hasType('clothes') || hasType('toys') || hasType('caps')) && renderMarkers(collectsData.places, 'charity', setSelectedPlace)}
-    {(hasType('meds')) && renderMarkers(expiredDrugsData.places, 'pharmacy', setSelectedPlace)}
-    {(hasType('batteries')) && renderMarkers(batteriesData.places, 'battery', setSelectedPlace)}
-    {(hasType('electro')) && renderMarkers(wasteData.places, 'waste', setSelectedPlace)}
-    {/*{(hasType('electro')) && renderMarkers(realEstateData.places, 'glass', setSelectedPlace)}*/}
+  return (
+    <GoogleMap defaultZoom={14} defaultCenter={mapCenter}>
+      {(hasType("clothes") || hasType("toys") || hasType("caps")) &&
+        renderMarkers(collectsData.places, "charity", setSelectedPlace)}
+      {hasType("meds") &&
+        renderMarkers(expiredDrugsData.places, "pharmacy", setSelectedPlace)}
+      {hasType("batteries") &&
+        renderMarkers(batteriesData.places, "battery", setSelectedPlace)}
+      {hasType("electro") &&
+        renderMarkers(wasteData.places, "waste", setSelectedPlace)}
+      {/*{(hasType('electro')) && renderMarkers(realEstateData.places, 'glass', setSelectedPlace)}*/}
 
-    { !types && renderMarkers(collectsData.places, 'charity', setSelectedPlace) }
-    { !types && renderMarkers(expiredDrugsData.places, 'pharmacy', setSelectedPlace)}
-    { !types && renderMarkers(batteriesData.places, 'battery', setSelectedPlace)}
-    { !types && renderMarkers(wasteData.places, 'waste', setSelectedPlace)}
-    { !types && renderMarkers(realEstateData.places, 'glass', setSelectedPlace)}
+      {!types &&
+        renderMarkers(collectsData.places, "charity", setSelectedPlace)}
+      {!types &&
+        renderMarkers(expiredDrugsData.places, "pharmacy", setSelectedPlace)}
+      {!types &&
+        renderMarkers(batteriesData.places, "battery", setSelectedPlace)}
+      {!types && renderMarkers(wasteData.places, "waste", setSelectedPlace)}
+      {!types &&
+        renderMarkers(realEstateData.places, "glass", setSelectedPlace)}
 
-    {selectedPlace && (
-      <InfoWindow
-        position={{lat: selectedPlace.location.lat, lng: selectedPlace.location.lng}}
-        onCloseClick={() => setSelectedPlace(null)}
-      >
-        <div style={{padding: '10px'}}>
-          <h2>{getPlaceName(selectedPlace)}</h2>
-          <h4>{selectedPlace.address}</h4>
-        </div>
-      </InfoWindow>
-    )}
-  </GoogleMap>
-}
+      {selectedPlace && (
+        <InfoWindow
+          position={{
+            lat: selectedPlace.location.lat,
+            lng: selectedPlace.location.lng,
+          }}
+          onCloseClick={() => setSelectedPlace(null)}
+        >
+          <div style={{ padding: "10px" }}>
+            <h2>{getPlaceName(selectedPlace)}</h2>
+            <h4>{selectedPlace.address}</h4>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+};
 
 const getPlaceName = (place) => {
-  return place.place || (place.administrator
-    ? `Miejsce gromadzenia odpadów MGO, administrator ${place.administrator}`
-    : 'KSON i szkło');
-}
+  return (
+    place.place ||
+    (place.administrator
+      ? `Miejsce gromadzenia odpadów MGO, administrator ${place.administrator}`
+      : "KSON i szkło")
+  );
+};
 
 const renderMarkers = (data, icon, onClick) => {
   return data
-    .filter(place => !!place.location)
-    .map(place => (
+    .filter((place) => !!place.location)
+    .map((place) => (
       <Marker
         key={generateKey()}
-        position={{lat: place.location.lat, lng: place.location.lng}}
+        position={{ lat: place.location.lat, lng: place.location.lng }}
         onClick={() => onClick(place)}
         icon={{
           url: `markers/${icon}.png`,
-          scaledSize: new window.google.maps.Size(25, 25)
+          scaledSize: new window.google.maps.Size(25, 25),
         }}
       />
     ));
-}
+};
 
 const generateKey = () => Math.random().toString(36).substring(7);
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-const Find = ({types}) => {
-  return <div style={{width: '100%', height: '100vh'}}>
-    <WrappedMap
-      googleMapURL={googleMapURL}
-      loadingElement={<div style={{height: '100%'}}/>}
-      containerElement={<div style={{height: '100%'}}/>}
-      mapElement={<div style={{height: '100%'}}/>}
-      types={types}
-    />
-  </div>;
+const Find = ({ types }) => {
+  return (
+    <div style={{ width: "100%", height: "80vh" }}>
+      <Header />
+      <WrappedMap
+        googleMapURL={googleMapURL}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+        types={types}
+      />
+    </div>
+  );
 };
 
 export default Find;
